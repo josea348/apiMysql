3c60a0588b2f6ac048ca018b76bc2e7ef1df4607
josea348 <jv1424937@gmail.com>
2024-02-29 12:48:10 -0500

    actualizo rama

--- src/validate/usuarios.js
+++ src/validate/usuarios.js
@@ -1,6 +1,6 @@
 import { check } from "express-validator";
 
 export const validarUsuario = [
-    check('nombres', 'El nombre es obligatorio o maximo 50 caracteres').not().isEmpty().isLength({max:50}),
+    check('nombre', 'El nombre es obligatorio o maximo 50 caracteres').not().isEmpty().isLength({max:50}),
     check('correo', 'correo invalido').isEmail()
 ];
